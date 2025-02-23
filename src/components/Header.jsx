ecf0f7ed3ea7ce6dd9a3a7b98bddc413e0ad71a2
rhenzala <97076128+rhenzala@users.noreply.github.com>
2025-02-23 21:14:10 +0800

    Add components and header structure

---
+++ src/components/Header.jsx
@@ -0,0 +1,20 @@
+const Header = () => {
+    return (
+       <header className="h-[70px] border-b-cyan border-b border-b-solid flex justify-between items-center px-12">
+            <div>
+                <button>
+                    <a href="" className="text-cyan font-mono text-xl font-bold">rhenzala</a>
+                </button>
+            </div>
+            <nav>
+                <ul className="styled-list flex gap-8 text-sm">
+                    <li className="hover:text-cyan font-bold text-slate"><a href="">About</a></li>
+                    <li className="hover:text-cyan font-bold text-slate"><a href="">Projects</a></li>
+                    <li className="hover:text-cyan font-bold text-slate"><a href="">Contact</a></li>
+                </ul>
+            </nav>
+       </header>
+    )
+}
+
+export default Header
