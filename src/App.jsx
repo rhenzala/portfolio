18bd804662ae1c15c0fbbbb15f7b00498951a6c7
rhenzala <97076128+rhenzala@users.noreply.github.com>
2025-02-24 11:49:58 +0800

    Add Main jsx component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import Header  from './components/Header.jsx'
+import Main from './components/Main.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -8,6 +9,7 @@ function App() {
   return (
     <div>
       <Header />
+      <Main />
     </div>
   )
 }
