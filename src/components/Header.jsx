f29c353979447fb7ba30b52019f32ff2b7de72d6
rhenzala <97076128+rhenzala@users.noreply.github.com>
2025-02-25 14:45:36 +0800

    Change text color of name, description

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,8 +4,8 @@ const Header = () => {
     return (
        <header className="lg:w-[48%] lg:py-24 lg:sticky lg:flex lg:top-0 lg:max-h-screen lg:flex-col lg:justify-between">
             <div>
-                <h1 className="text-4xl font-bold tracking-tight sm:text-5xl">Rhenzal Abdulsali</h1>
-                <h2 className="text-slate-500 text-xl font-semibold mt-3 tracking-tight">Aspiring DevOps Engineer</h2>
+                <h1 className="text-4xl font-bold tracking-tight sm:text-5xl text-slate-200">Rhenzal Abdulsali</h1>
+                <h2 className="text-slate-300 text-xl font-semibold mt-3 tracking-tight">Aspiring DevOps Engineer</h2>
                 <p className="mt-4 max-w-xs leading-normal">I am a new graduate aiming to be a DevOps.</p>
                 
             </div>
