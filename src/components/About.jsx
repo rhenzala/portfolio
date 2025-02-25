e3d9cdfd37188f7f773fdc18a297e0a228f4dcca
rhenzala <97076128+rhenzala@users.noreply.github.com>
2025-02-25 15:13:50 +0800

    Add initial layout and content

---
+++ src/components/About.jsx
@@ -0,0 +1,12 @@
+const About = () => {
+    return (
+        <section id="about" className="mb-16 scroll-mt-16 md:mb-24 lg:mb-36 lg:scroll-mt-24" aria-label="About me">
+            <h2 className="text-xl text-slate-200 tracking-tight font-semibold mb-8 uppercase">About Me</h2>
+            <p className="mb-4">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quaerat, a dolor repellendus repellat inventore est! Voluptates consequuntur voluptas at dignissimos in magnam repellat animi voluptatum. Minus sed iste soluta reiciendis.</p>
+            <p className="mb-4">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quaerat, a dolor repellendus repellat inventore est! Voluptates consequuntur voluptas at dignissimos in magnam repellat animi voluptatum. Minus sed iste soluta reiciendis.</p>
+            <p className="mb-4">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quaerat, a dolor repellendus repellat inventore est! Voluptates consequuntur voluptas at dignissimos in magnam repellat animi voluptatum. Minus sed iste soluta reiciendis.</p>
+        </section>
+    )
+}
+
+export default About
